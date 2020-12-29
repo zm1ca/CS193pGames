0a12003730fa59cb25117d3363c389413a67ed03
zm1ca <zmicier.fied@yandex.ru>
2020-12-29 14:15:56 +0300

    Initial Commit

---
+++ CS193pGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS193pGames
+//
+//  Created by Źmicier Fiedčanka on 29.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
